3c4e8b2de991898e6f9fba8802bfda26300c0791
asfnnn <asadarifin28@gmail.com>
2025-06-25 12:32:11 +0800

    add: fungsi menu

--- Gunshop.cpp
+++ Gunshop.cpp
@@ -2,6 +2,18 @@
 #include <string>
 using namespace std;
 
+void menu()
+{
+    cout << "\n=== GUN SHOP MENU ===\n";
+    cout << "1. Tambah senjata\n";
+    cout << "2. Tampilkan semua senjata\n";
+    cout << "3. Cari senjata\n";
+    cout << "4. Hapus senjata\n";
+    cout << "5. Total semua stok\n";
+    cout << "6. Hapus semua data\n";
+    cout << "0. Keluar\n";
+    cout << "Pilih: ";
+}
 
 int main()
 {
